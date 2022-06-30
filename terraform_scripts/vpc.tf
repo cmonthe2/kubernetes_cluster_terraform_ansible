1c4cd3027e04e6f0ce126382f79b1163e84fb846
cmonthe2 <88361315+cmonthe2@users.noreply.github.com>
2022-06-29 23:09:36 -0400

    Update vpc.tf

--- terraform_scripts/vpc.tf
+++ terraform_scripts/vpc.tf
@@ -7,29 +7,29 @@ resource "aws_vpc" "kubernetes" {
     Name = "kubernetes"
   }
 }
-resource "aws_internet_gateway" "kubernetes_vpc_igw" {
-  vpc_id = aws_vpc.kubernetes.vpc_id
-tags = {
-Name = "kubernetes_vpc_igw"
-}
-}
+#resource "aws_internet_gateway" "kubernetes_vpc_igw" {
+ # vpc_id = aws_vpc.kubernetes.vpc_id
+#tags = {
+#Name = "kubernetes_vpc_igw"
+#}
+#}
 
-resource "aws_subnet" "kubernetes_subnets" {
-count                    = length(var.subnets_cidr)
-vpc_id                   = aws_vpc.kubernetes.vpc_id
-cidr_block               = element(var.subnets_cidr, count.index)
-availability_zone        = element(var.availability_zones, count.index)
-map_public_ip_on_launch  = true
-tags = {
-Name = "kubernetes_subnets_${count.index + 1}"
-}
-}
+#resource "aws_subnet" "kubernetes_subnets" {
+#count                    = length(var.subnets_cidr)
+#vpc_id                   = aws_vpc.kubernetes.vpc_id
+#cidr_block               = element(var.subnets_cidr, count.index)
+#availability_zone        = element(var.availability_zones, count.index)
+#map_public_ip_on_launch  = true
+#tags = {
+#Name = "kubernetes_subnets_${count.index + 1}"
+#}
+#}
 
-resource "aws_route_table" "kubernetes_public_rt" {
-vpc_id = aws_vpc.kubernetes.vpc_id
+resource "aws_route_table" "kubernetes_public_route_table" {
+vpc_id = "aws_vpc.kubernetes.vpc_id"
 route {
 cidr_block = "0.0.0.0/0"
-gateway_id = aws_internet_gateway.kubernetes_vpc_igw.id
+gateway_id = "aws_internet_gateway.kubernetes_vpc_igw.id"
 }
 tags = {
 Name = "kubernetes_vpc_public_rt"
@@ -37,14 +37,8 @@ Name = "kubernetes_vpc_public_rt"
 }
 
 
-resource "aws_route_table-association" "rt_sub_assocaition" {
-count   = length(var.subnets_cidr)
-subnet_id  = element(aws_subnet.kubernetes_subnets.*.id, count.index)
-route_table_id = aws_route_table.kubernetes_public_rt.id
-}
-
-
-
-
-
-}
+# resource "aws_route_table_association" "rt_sub_assocaition" {
+# count   = length(var.subnets_cidr)
+# subnet_id  = element(aws_subnet.kubernetes_subnets.*.id, count.index)
+# route_table_id = aws_route_table.kubernetes_public_rt.id
+# }
